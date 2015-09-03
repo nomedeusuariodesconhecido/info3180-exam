a7015806b64a6ee124d1daca563b828c4b2ee2f2
nomedeusuario <jason.swaby@mymona.uwi.edu>
2015-09-03 17:10:18 +0000

    Update 18: Flexible Form Data Entry

--- flask_app/app/views.py
+++ flask_app/app/views.py
@@ -32,6 +32,8 @@ def about():
 def displayEmails():
     #query_result = []
     error = "Sorry... No such person!"
+    empty = "Please enter a name in the search boxes above."
+    
     if request.method =='POST':
         f_name = request.form["first_name"]
         l_name = request.form["last_name"]
@@ -39,15 +41,19 @@ def displayEmails():
         if f_name and l_name:
             f_name = f_name[0].upper() + f_name[1:len(f_name)]
             l_name = l_name[0].upper() + l_name[1:len(l_name)]
-            query_result = db.session.query(Emails).filter((Emails.frst_name==f_name) | (Emails.lst_name==l_name)).first()
-            
+            query_result = db.session.query(Emails).filter(getattr(Emails, "frst_name").like("%" + f_name + "%") | getattr(Emails, "lst_name").like("%" + l_name + "%")).first()
+            # query_result = db.session.query(Emails).filter((Emails.frst_name==f_name) | (Emails.lst_name==l_name)).first()
         elif f_name:
             f_name = f_name[0].upper() + f_name[1:len(f_name)]
-            query_result = db.session.query(Emails).filter(Emails.frst_name==f_name).first()
+            query_result = db.session.query(Emails).filter(getattr(Emails, "frst_name").like("%" + f_name + "%")).first()
+            # query_result = db.session.query(Emails).filter(Emails.frst_name==f_name).first()
             
         elif l_name:
-            l_name = l_name[0].upper() + l_name[1:len(l_name)]            
-            query_result = db.session.query(Emails).filter(Emails.lst_name==l_name).first()
+            l_name = l_name[0].upper() + l_name[1:len(l_name)]  
+            query_result = db.session.query(Emails).filter(getattr(Emails, "lst_name").like("%" + l_name + "%")).first()
+            # query_result = db.session.query(Emails).filter(Emails.lst_name==l_name).first()
+        else:
+            return render_template("ContactsForm.html", empty=empty)
             
         # query_result_f_name = db.session.query(Emails).filter(getattr(Emails, "frst_name").like("%" + f_name + "%")).first()
         # query_result_l_name = db.session.query(Emails).filter(getattr(Emails, "lst_name").like("%" + l_name + "%")).first()
@@ -61,7 +67,6 @@ def displayEmails():
         return render_template('ContactsForm.html', firstname=f_name, lastname=l_name, usr_email=email)
     else:
         return render_template('ContactsForm.html', error=error)
-
 
 
 # @app.route('/displayEmails/<name>', methods=["GET", "POST"])
